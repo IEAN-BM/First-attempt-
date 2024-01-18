48eee61b84530fbf292c9d40073714f49b4e10ae
IEAN-BM <153981347+IEAN-BM@users.noreply.github.com>
2024-01-18 19:50:11 +0100

    Update NO-13.py

--- Second 10/NO-13.py
+++ Second 10/NO-13.py
@@ -1,5 +1,5 @@
-L = []
-n = eval(input('enter limit'))
+L = [0]
+n = eval(input('enter limit or range'))
 for i in range(0,n):
     L.append(input('enter a number'))
 print('Maximum element is',max(L))
